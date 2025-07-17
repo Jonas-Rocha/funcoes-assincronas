f84c268c494cd0eb85a8c43208a84806ce2186dd
Jonas Rocha <jonasrpnstudy@gmail.com>
2025-07-17 14:30:41 -0300

    Conhecendo o async e await

--- script.js
+++ script.js
@@ -14,22 +14,52 @@ function asyncFunction() {
     }, 3000); // Simula uma operação que leva 3 segundos.
   });
 }
+// // Visualizando que o retorno é uma promise.
+// //console.log(asyncFunction());
 
-// Visualizando que o retorno é uma promise.
-//console.log(asyncFunction());
+// console.log("Executando função assincrona...");
 
-console.log("Executando função assincrona...");
+// // const response = asyncFunction();
+// // console.log(response);
 
-// const response = asyncFunction();
-// console.log(response);
+// asyncFunction()
+//   .then((response) => {
+//     console.log("Sucesso:", response);
+//   })
+//   .catch((error) => {
+//     console.log("Error:", error);
+//   })
+//   .finally(() => {
+//     console.log("Fim da execução");
+//   });
 
-asyncFunction()
-  .then((response) => {
+// O CÓDIGO ACIMA É UMA FORMA DE LIDAR SEM O ASYNC AWAIT. ONDE USAMOS O THEN PARA TRATAR A RESOLUÇÃO E O CATCH PARA TRATAR O ERRO.
+
+// async function fetch() {
+//   const response = await asyncFunction(); // o await não funciona sem o async.
+//   console.log(response);
+// }
+// fetch();
+
+// usando com arrow function
+// const fetch = async () => {
+//   const response = await asyncFunction();
+
+// };
+
+// fetch();
+
+// usando com arrow function e bloco de try/catch
+
+const fetch = async () => {
+  try {
+    const response = await asyncFunction();
     console.log("Sucesso:", response);
-  })
-  .catch((error) => {
-    console.log("Error:", error);
-  })
-  .finally(() => {
-    console.log("Fim da execução");
-  });
+  } catch (error) {
+    console.log("Erro:", error);
+  } finally {
+    console.log("Fim da execução!");
+  }
+};
+
+fetch();
